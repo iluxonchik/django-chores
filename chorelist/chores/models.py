427b39a8670ec70b17154ca3cf07333a6f4829be
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-05-26 21:54:37 +0100

    feat: add and register models in admin.py
    * add ChoreList model to admin.py
    * add Chore model to admin.py
    * Chore creation is shown inline when managing a ChoreList

--- chorelist/chores/models.py
+++ chorelist/chores/models.py
@@ -5,9 +5,15 @@ from django.db import models
 class ChoreList(models.Model):
 	name = models.CharField(max_length=100)
 	due_date = models.DateTimeField()
+	
+	def __str__(self):
+		return self.name
 
 class Chore(models.Model):
 	chore_list = models.ForeignKey(ChoreList)
 	name = models.CharField(max_length=100)
 	due_date = models.DateTimeField()
 	complete = models.BooleanField(default=False)
+	
+	def __str__(self):
+		return self.name
