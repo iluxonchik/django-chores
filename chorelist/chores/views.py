285a182efa13f8e472e17fd6f88eb58a884ad71b
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-05-28 22:28:44 +0100

    feat: add simple view

--- chorelist/chores/views.py
+++ chorelist/chores/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# In Django, a View is just a function that returns an HTTP response
+
+def index(request):
+    # request represents an HTTP request that comes in
+    return HttpResponse("Hello from the chores app")
