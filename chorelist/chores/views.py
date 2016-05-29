cfacf25fbabb04326ac96a3d026ca92cfef86314
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-05-29 21:57:24 +0100

    feat: additions to detail template; 404 refactoring

--- chorelist/chores/views.py
+++ chorelist/chores/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import ChoreList
 
@@ -22,11 +22,15 @@ def detail(request, chorelist_id):
     # note the named pattern from urls.py, it has the same name as the 2nd parameter of this function,
     # which is how the id in the url will be mapped to chorelist_id parameter of this function
     # (?P<chorelist_id>[0-9]+)
+    lst = get_object_or_404(ChoreList, pk=chorelist_id)
+    return render(request, 'chores/detail.html', {'chorelist':lst})
+    """
     try:
         lst = ChoreList.objects.get(pk=chorelist_id)
     except ChoreList.DoesNotExist:
         raise Http404('Chore list with id {0} does not exist'.format(chorelist_id)) # message only shown in Debug mode
     return render(request, 'chores/detail.html', {'chorelist' : lst})
+    """
     
 def chores(request, chorelist_id):
     return HttpResponse("You're looking at the Chores from ChoreList #{0}".format(chorelist_id))
