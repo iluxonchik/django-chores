e2756e16c5e5ff4c94faa5563f2d0efeaf30ef0d
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-05-29 20:40:23 +0100

    feat: add detail, chores and choredetail views, with parameters

--- chorelist/chores/urls.py
+++ chorelist/chores/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<chorelist_id>[0-9]+)/$', views.detail, name='detail'), # NOTE: naming of named regex is important (see views.py for more)
+    url(r'^(?P<chorelist_id>[0-9]+)/chores/$', views.chores, name='chores'),
+    url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/$', views.choredetail, name='choredetail'),
+    
 ]
--- chorelist/chores/views.py
+++ chorelist/chores/views.py
@@ -6,4 +6,16 @@ from django.http import HttpResponse
 
 def index(request):
     # request represents an HTTP request that comes in
-    return HttpResponse("Hello from the chores app")
+    return HttpResponse("You're at the ChoreList index")
+    
+def detail(request, chorelist_id):
+    # note the named pattern from urls.py, it has the same name as the 2nd parameter of this function,
+    # which is how the id in the url will be mapped to chorelist_id parameter of this function
+    # (?P<chorelist_id>[0-9]+)
+    return HttpResponse("You're looking at ChoreList #{0}".format(chorelist_id))
+    
+def chores(request, chorelist_id):
+    return HttpResponse("You're looking at the Chores from ChoreList #{0}".format(chorelist_id))
+    
+def choredetail(request, chorelist_id, chore_id):
+    return HttpResponse("You're looking at the Chore #{0} from ChoreList #{1}".format(chore_id, chorelist_id))
