784aefb010bdfd1cba6aa99d880d2d8b43862320
ILUXONCHIK <iluxon4ik@hotmail.com>
2016-05-30 13:02:40 +0100

    feat: add update and new list creation forms
    Basically, playing with POST requests

--- chorelist/chores/urls.py
+++ chorelist/chores/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^newlist/$', views.newlist, name='newlist'),
     url(r'^(?P<chorelist_id>[0-9]+)/$', views.detail, name='detail'), # NOTE: naming of named regex is important (see views.py for more)
-    url(r'^(?P<chorelist_id>[0-9]+)/chores/$', views.chores, name='chores'),
     url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/$', views.choredetail, name='choredetail'),
+    url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/update/$', views.updatechore, name='updatechore'),
     
 ]
--- chorelist/chores/views.py
+++ chorelist/chores/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from .models import ChoreList
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import ChoreList, Chore
 
 # Create your views here.
 # In Django, a View is just a function that returns an HTTP response
@@ -18,6 +18,13 @@ def index(request):
     return HttpResponse(template.render(context)) # render template with the specified context
     """
     
+def newlist(request):
+    if 'name' in request.POST and 'duedate' in request.POST:
+        list = ChoreList(name=request.POST['name'], due_date=request.POST['duedate'])
+        list.save()
+        return HttpResponseRedirect('/chores/')
+    return render(request, 'chores/newlist.html', {})
+
 def detail(request, chorelist_id):
     # note the named pattern from urls.py, it has the same name as the 2nd parameter of this function,
     # which is how the id in the url will be mapped to chorelist_id parameter of this function
@@ -31,9 +38,19 @@ def detail(request, chorelist_id):
         raise Http404('Chore list with id {0} does not exist'.format(chorelist_id)) # message only shown in Debug mode
     return render(request, 'chores/detail.html', {'chorelist' : lst})
     """
-    
-def chores(request, chorelist_id):
-    return HttpResponse("You're looking at the Chores from ChoreList #{0}".format(chorelist_id))
-    
+        
 def choredetail(request, chorelist_id, chore_id):
-    return HttpResponse("You're looking at the Chore #{0} from ChoreList #{1}".format(chore_id, chorelist_id))
+    list = get_object_or_404(ChoreList, pk=chorelist_id)
+    chore = get_object_or_404(Chore, pk=chore_id)
+    return render(request, 'chores/choredetail.html', {'chorelist':list, 'chore':chore})
+    
+def updatechore(request, chorelist_id, chore_id):
+    chore = get_object_or_404(Chore, pk=chore_id)
+    
+    if 'complete' in request.POST:
+        chore.complete = True
+    else:
+        chore.complete = False
+    
+    chore.save()
+    return HttpResponseRedirect('/chores/' + chorelist_id + '/chores/' + chore_id)
